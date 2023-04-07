5faec6dfa86c29a1af6d2f7912e08f5db2d7dec5
Shreedevib <122557120+Shreedevib@users.noreply.github.com>
2023-04-07 06:58:56 -0700

    Add files via upload

---
+++ Azureterraform/Provider.tf
@@ -0,0 +1,15 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  #client_id       = ""
+  #client_secret   = ""
+  #tenant_id       = ""
+  #subscription_id = ""
+}
+
+# $env:ARM_CLIENT_ID = "00000000-0000-0000-0000-000000000000"
+# $env:ARM_CLIENT_SECRET = "12345678-0000-0000-0000-000000000000"
+# $env:ARM_TENANT_ID = "10000000-0000-0000-0000-000000000000"
+# $env:ARM_SUBSCRIPTION_ID = "20000000-0000-0000-0000-000000000000"
+
+#dir env:
---
+++ Azureterraform/rg.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "adhiti01" {
+  name     = "adhiti01"
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "adhiti02" {
+  name     = "adhiti02"
+  location = "East US"
+}
